fd3efc7eb8042cbd7291c6a52227d30c317dbf0a
seyeon112 <lilylilyghost@gmail.com>
2024-10-01 17:44:12 +0900

    jsp day01

---
+++ src/main/java/com/app/servlet/MyServlet.java
@@ -0,0 +1,41 @@
+package com.app.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public MyServlet() {
+        super();
+    
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		
+		System.out.println(name);
+		
+		out.print("<h1>Hello, Jsp</h1>");
+		out.print("<h1>안녕!</h1>");
+		out.close();
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/intro.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%-- 	<%
+		for(int i = 0; i < 10; i ++){
+	%>		
+		<h1>안녕하세요!</h1>>
+	<% 
+		}
+	%> --%>
+	
+	<form action="/home" method="post">
+		<input name="name" placeholder="이름을 입력하세요" />
+		<button>전송</button>
+	</form>
+	
+	
+</body>
+</html>
